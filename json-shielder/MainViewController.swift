8e2663c70763dfc56bea226af1b5d2e10e79554b
Maxim Skryabin <mxmskrbn@gmail.com>
2021-11-23 12:35:48 +0300

    Removed unused files

--- json-shielder/MainViewController.swift
+++ json-shielder/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  json-shielder
 //
 //  Created by Maxim Skryabin on 23.11.2021.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    view.backgroundColor = .red
   }
-
-
 }
-
